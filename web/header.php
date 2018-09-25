90e73b317ea8501d531f959b005c6822038a5c51
rachelbarnes <bar13059@byui.edu>
2018-09-25 15:06:45 -0600

    moved header to separate file

---
+++ web/header.php
@@ -0,0 +1,12 @@
+<header>
+    <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+        <a class="navbar-brand" href="#">Home</a>
+        <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+        <div class="navbar-collapse collapse show" id="navbarCollapse">
+            <ul class="navbar-nav mr-auto">
+                <li nav-item>
+                    <a class="nav-link" href="homework.php">Homework</a></li>
+            </ul>
+        </div>
+    </nav>
+</header>
--- web/index.php
+++ web/index.php
@@ -10,18 +10,7 @@
 </head>
 
 <body>
-    <header>
-        <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-            <a class="navbar-brand" href="#">Home</a>
-            <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
-            <div class="navbar-collapse collapse show" id="navbarCollapse">
-                <ul class="navbar-nav mr-auto">
-                    <li nav-item>
-                        <a class="nav-link" href="homework.php">Homework</a></li>
-                </ul>
-            </div>
-        </nav>
-    </header>
+    <?php include $_SERVER['DOCUMENT_ROOT'] . 'header.php'; ?>
     <main role="main">
         <section class="jumbotron text-center">
             <div class="container">
