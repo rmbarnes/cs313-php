e671318b8e2ad9b357cdbcf56bdfe5603556bf99
rachelbarnes <bar13059@byui.edu>
2018-09-25 15:32:06 -0600

    fixed homepage link

--- web/header.php
+++ web/header.php
@@ -1,6 +1,6 @@
 <header>
     <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-        <a class="navbar-brand" href="#">Home</a>
+        <a class="navbar-brand" href="index.php">Home</a>
         <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
         <div class="navbar-collapse collapse show" id="navbarCollapse">
             <ul class="navbar-nav mr-auto">
