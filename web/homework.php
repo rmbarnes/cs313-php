6b3a9ec225299383f2a2212ce5a27784762a76c5
rachelbarnes <bar13059@byui.edu>
2018-09-25 15:27:58 -0600

    basic edits

--- web/homework.php
+++ web/homework.php
@@ -6,11 +6,14 @@
         <link rel="stylesheet" type="text/css" href="style.css"/>
     </head>
     <body>
-        <p></p>
+        <?php include 'header.php'; ?>
+        <main role="main">
+            <section class="jumbotron text-center">
+                <div class="container">
+                    <h1 class="jumbotron-heading">Coming Soon!</h1>
+                </div>
+            </section>
+        </main>
     </body>
 </html>
-<?php
 
-phpinfo();
-
-?>
--- web/index.php
+++ web/index.php
@@ -1,24 +1,19 @@
 <!DOCTYPE HTML>
 <html lang="en-us">
-
 <head>
     <meta charset="utf-8">
-    <title>Hello World</title>
+    <title>Rachel's Homepage</title>
     <link rel="stylesheet" type="text/css" href="bootstrap-4.1.3/dist/css/bootstrap.min.css" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="bootstrap-4.1.3/site/docs/4.1/examples/carousel/carousel.css" />
 </head>
-
 <body>
     <?php include 'header.php'; ?>
     <main role="main">
         <section class="jumbotron text-center">
             <div class="container">
                 <h1 class="jumbotron-heading">Welcome!</h1>
-                <p class="lead text-muted">You've landed upon my new homepage! How fortunate.</p>
-                <p>
-                    <a href="#" class="btn btn-primary my-2">Placeholder button</a>
-                </p>
+                <p class="lead text-muted">You've landed upon my homepage! How fortunate. Below, you'll find a few images from my recent trip to Europe. I love traveling and exploring new places.</p>
             </div>
         </section>
         <div class="album py-5">
@@ -29,7 +24,7 @@
                         <div class="card mb-4 shadow-sm">
                             <img class="card-img-top" alt="Thumbnail [100%x225]" style="height: 225px; width: 100%; display: block;" src="images/IMG_1306.JPG" data-holder-rendered="true">
                             <div class="card-body">
-                                <p class="card-text">This is from a stop in Venice.</p>
+                                <p class="card-text">This is from our stop in Venice.</p>
                             </div>
                         </div>
                     </div>
@@ -37,7 +32,7 @@
                         <div class="card mb-4 shadow-sm">
                             <img class="card-img-top" alt="Thumbnail [100%x225]" src="images/IMG_1034.JPG" data-holder-rendered="true" style="height: 225px; width: 100%; display: block;">
                             <div class="card-body">
-                                <p class="card-text">The great duomo of Florence</p>
+                                <p class="card-text">The great Duomo of Florence</p>
                             </div>
                         </div>
                     </div>
@@ -65,10 +60,3 @@
 </body>
 
 </html>
-<!--
-<?php
-
-  phpinfo();
-
-?>
--->
