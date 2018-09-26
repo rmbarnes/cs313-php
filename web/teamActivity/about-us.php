be931bd70847b3b96b7d16d7405cd9bee405bf4a
rachelbarnes <bar13059@byui.edu>
2018-09-26 16:54:16 -0600

    fixed style link

--- web/teamActivity/about-us.php
+++ web/teamActivity/about-us.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <title>Cookies | About Us</title>
-        <link rel="stylesheet" type="text/css" href="bootstrap-4.1.3/dist/css/bootstrap.min.css" />
+        <link rel="stylesheet" type="text/css" href="../bootstrap-4.1.3/dist/css/bootstrap.min.css" />
     </head>
     <body>
         <?php include 'teamHeader.php';?>
