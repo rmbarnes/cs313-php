edf8d5b2fe9f321ca4f68a7e96222febaa9f968a
rachelbarnes <bar13059@byui.edu>
2018-09-26 22:08:04 -0600

    added php to header for active page

--- web/teamActivity/teamHeader.php
+++ web/teamActivity/teamHeader.php
@@ -1,14 +1,17 @@
+<?php
+$file = pathinfo($_SERVER['PHP_SELF'], PATHINFO_FILENAME);
+?>
 <header>
     <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
         <ul class="navbar-nav mr-auto">
-            <li class="nav-item">
-                <a class="nav-link active" href="/teamActivity/home.php" data-toggle="tab">Home</a>
+            <li class="nav-item <?php if ($file === 'home') echo 'active'?>">
+                <a class="nav-link" href="/teamActivity/home.php" data-toggle="tab">Home</a>
             </li>
-            <li class="nav-item">
-                <a class="nav-link active" href="/teamActivity/about-us.php" data-toggle="tab">About Us</a>
+            <li class="nav-item <?php if ($file === 'about') echo 'active'?>">
+                <a class="nav-link" href="/teamActivity/about-us.php" data-toggle="tab">About Us</a>
             </li>
-            <li class="nav-item">
-                <a class="nav-link active" href="/teamActivity/login.php" data-toggle="tab">Login</a>
+            <li class="nav-item <?php if ($file === 'login') echo 'active'?>">
+                <a class="nav-link" href="/teamActivity/login.php" data-toggle="tab">Login</a>
             </li>
         </ul>
     </nav>
