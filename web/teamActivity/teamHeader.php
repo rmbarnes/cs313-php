a9fbe588c6535f6271121a28228d05c6c76ee4e4
rachelbarnes <bar13059@byui.edu>
2018-09-26 22:29:33 -0600

    took out button and script tags

--- web/teamActivity/teamHeader.php
+++ web/teamActivity/teamHeader.php
@@ -2,9 +2,6 @@
 <header>
     <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
         <a class="navbar-brand" href="index.php">Cookie Family</a>
-        <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
         <div class="navbar-collapse collapse" id="navbarCollapse" style="">
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item <?php if ($file === 'home') echo 'active'?>">
