0ccba632b9942df73face32708cad16661d0ef20
rachelbarnes <bar13059@byui.edu>
2018-09-26 21:41:28 -0600

    reformatted login page

--- web/teamActivity/login.php
+++ web/teamActivity/login.php
@@ -1,13 +1,22 @@
 <!DOCTYPE html>
 <html lang="en-us">
-    <head>
-        <meta charset="utf-8">
-        <title>Cookies | Login</title>
-        <link rel="stylesheet" type="text/css" href="../bootstrap-4.1.3/dist/css/bootstrap.min.css" />
-    </head>
-    <body>
-        <?php include 'teamHeader.php';?>
-        <button type="button" onclick="">Login as Administrator</button>
-        <button type="button" onclick="">Login as Tester</button>
-    </body>
+
+<head>
+    <meta charset="utf-8">
+    <title>Cookies | Login</title>
+    <link rel="stylesheet" type="text/css" href="../bootstrap-4.1.3/dist/css/bootstrap.min.css" />
+</head>
+
+<body>
+    <?php include 'teamHeader.php';?>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <button type="button" onclick="">Login as Administrator</button>
+                <button type="button" onclick="">Login as Tester</button>
+            </div>
+        </div>
+    </div>
+</body>
+
 </html>
