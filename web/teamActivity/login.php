e457bfd9f0c64cf7fa33c6c2c8b4c7d5496dbd6d
rachelbarnes <bar13059@byui.edu>
2018-09-26 21:56:59 -0600

    fixed padding-top

--- web/teamActivity/login.php
+++ web/teamActivity/login.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <title>Cookies | Login</title>
     <link rel="stylesheet" type="text/css" href="../bootstrap-4.1.3/dist/css/bootstrap.min.css" />
-    <link rel="stylesheet" type="text/css" href="../style.css">
+    <link rel="stylesheet" type="text/css" href="teamStyle.css">
 </head>
 
 <body>
