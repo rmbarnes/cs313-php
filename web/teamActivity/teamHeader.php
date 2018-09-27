8b622f5fb1471ce134f30f71cb1f9ce0cca3c486
rachelbarnes <bar13059@byui.edu>
2018-09-26 22:25:49 -0600

    added php back in

--- web/teamActivity/teamHeader.php
+++ web/teamActivity/teamHeader.php
@@ -1,3 +1,4 @@
+<?php $file = pathinfo($_SERVER['PHP_SELF'], PATHINFO_FILENAME);?>
 <header>
     <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
         <a class="navbar-brand" href="index.php">Cookie Family</a>
@@ -9,8 +10,8 @@
                 <li class="nav-item <?php if ($file === 'home') echo 'active'?>">
                     <a class="nav-link" href="/teamActivity/home.php" data-toggle="tab">Home</a>
                 </li>
-                <li class="nav-item <?php if ($file === 'about-us') echo 'active'?>">
-                    <a class="nav-link" href="/teamActivity/about-us.php" data-toggle="tab">About Us</a>
+                <li class="nav-item <?php if ($file === 'about') echo 'active'?>">
+                    <a class="nav-link" href="/teamActivity/about.php" data-toggle="tab">About Us</a>
                 </li>
                 <li class="nav-item <?php if ($file === 'login') echo 'active'?>">
                     <a class="nav-link" href="/teamActivity/login.php" data-toggle="tab">Login</a>
