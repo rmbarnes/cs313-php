7dca742e8ad5b7d7be4981e037f5f46076a4be2f
rachelbarnes <bar13059@byui.edu>
2018-09-26 22:10:19 -0600

    fixed navigation

--- web/teamActivity/teamHeader.php
+++ web/teamActivity/teamHeader.php
@@ -1,18 +1,20 @@
-<?php
-$file = pathinfo($_SERVER['PHP_SELF'], PATHINFO_FILENAME);
-?>
 <header>
     <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-        <ul class="navbar-nav mr-auto">
-            <li class="nav-item <?php if ($file === 'home') echo 'active'?>">
-                <a class="nav-link" href="/teamActivity/home.php" data-toggle="tab">Home</a>
-            </li>
-            <li class="nav-item <?php if ($file === 'about') echo 'active'?>">
-                <a class="nav-link" href="/teamActivity/about-us.php" data-toggle="tab">About Us</a>
-            </li>
-            <li class="nav-item <?php if ($file === 'login') echo 'active'?>">
-                <a class="nav-link" href="/teamActivity/login.php" data-toggle="tab">Login</a>
-            </li>
-        </ul>
+        <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="navbar-collapse collapse" id="navbarCollapse" style="">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item <?php if ($file === 'home') echo 'active'?>">
+                    <a class="nav-link" href="/teamActivity/home.php" data-toggle="tab">Home</a>
+                </li>
+                <li class="nav-item <?php if ($file === 'about-us') echo 'active'?>">
+                    <a class="nav-link" href="/teamActivity/about-us.php" data-toggle="tab">About Us</a>
+                </li>
+                <li class="nav-item <?php if ($file === 'login') echo 'active'?>">
+                    <a class="nav-link" href="/teamActivity/login.php" data-toggle="tab">Login</a>
+                </li>
+            </ul>
+        </div>
     </nav>
 </header>
