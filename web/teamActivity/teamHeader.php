1cd06ad9e4fa32e9c56a985531dc5c2375593932
rachelbarnes <bar13059@byui.edu>
2018-09-26 21:33:09 -0600

    added active class to links

--- web/teamActivity/teamHeader.php
+++ web/teamActivity/teamHeader.php
@@ -1,20 +1,15 @@
 <header>
     <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-        <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="navbar-collapse collapse" id="navbarCollapse" style="">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item">
-                    <a class="nav-link" href="/teamActivity/home.php" data-toggle="tab">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/teamActivity/about-us.php" data-toggle="tab">About Us</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/teamActivity/login.php" data-toggle="pill">Login</a>
-                </li>
-            </ul>
-        </div>
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+                <a class="nav-link active" href="/teamActivity/home.php" data-toggle="tab">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link active" href="/teamActivity/about-us.php" data-toggle="tab">About Us</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link active" href="/teamActivity/login.php" data-toggle="tab">Login</a>
+            </li>
+        </ul>
     </nav>
 </header>
