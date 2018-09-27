f4f36684896858d5e3ccd3e4490fcf00fc1a4128
rachelbarnes <bar13059@byui.edu>
2018-09-26 22:21:30 -0600

    added company name

--- web/teamActivity/teamHeader.php
+++ web/teamActivity/teamHeader.php
@@ -1,5 +1,6 @@
 <header>
     <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+        <a class="navbar-brand" href="index.php">Cookie Family</a>
         <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
