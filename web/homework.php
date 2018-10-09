ee0733defc780a2aec4d94c4a3adfe49dfc807a2
rachelbarnes <bar13059@byui.edu>
2018-10-08 22:18:07 -0600

    added link in assignments page

--- web/homework.php
+++ web/homework.php
@@ -1,22 +1,29 @@
 <!DOCTYPE HTML>
 <html lang="en-us">
-    <head>
-        <meta charset="utf-8">
-        <title>Homework</title>
-        <link rel="stylesheet" type="text/css" href="bootstrap-4.1.3/dist/css/bootstrap.min.css" />
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="bootstrap-4.1.3/site/docs/4.1/examples/carousel/carousel.css" />
-    </head>
-    <body>
-        <?php include 'header.php'; ?>
-        <main role="main">
-            <section class="jumbotron text-center">
-                <div class="container">
-                    <h1 class="jumbotron-heading">Assignments</h1>
-                    <a class="btn btn-outline-info" href="shopping-cart/browse.php">Shopping Cart</a>
-                </div>
-            </section>
-        </main>
-    </body>
-</html>
 
+<head>
+    <meta charset="utf-8">
+    <title>Homework</title>
+    <link rel="stylesheet" type="text/css" href="bootstrap-4.1.3/dist/css/bootstrap.min.css" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="bootstrap-4.1.3/site/docs/4.1/examples/carousel/carousel.css" />
+</head>
+
+<body>
+    <?php include 'header.php'; ?>
+    <main role="main">
+        <section class="jumbotron text-center">
+            <div class="container">
+                <h1 class="jumbotron-heading">Assignments</h1>
+            </div>
+            <div class="container">
+                <div class="row">
+                    <a class="btn btn-outline-info" href="shopping-cart/browse.php">Shopping Cart</a>
+
+                </div>
+            </div>
+        </section>
+    </main>
+</body>
+
+</html>
