da810e03b2ded834275014505457e8980e80ea22
rachelbarnes <bar13059@byui.edu>
2018-10-08 21:30:26 -0600

    added cart styling

--- web/shopping-cart/view-cart.php
+++ web/shopping-cart/view-cart.php
@@ -29,14 +29,27 @@ $removedItem = $_POST['remove'];
         </div>
         <form method="post" action="view-cart.php">
 
-                    <ul>
-                        <?php
-                            foreach($_SESSION['cart'] as $item)
-                            {
-                                echo "<li>$item <button type='submit' name='remove' value='$item'>Remove item</button></li>";
-                            }
-                        ?>
-                    </ul>
+            <ul>
+                <?php
+                    foreach($_SESSION['cart'] as $item)
+                    {
+
+                        echo "<div class='album py-5'>
+                            <div class='container'>
+                            <div class='row'>
+                            <div class='col-md-4'>
+                            <div class='card mb-4 shadow-sm'>
+                            <li>$item <button type='submit' class='.btn-danger' name='remove' value='$item'>Remove item</button></li>
+                            </div>
+                            </div>
+                            </div>
+                            </div>
+                            </div>"
+
+//                        echo "<li>$item <button type='submit' class='.btn-danger' name='remove' value='$item'>Remove item</button></li>";
+                    }
+                ?>
+            </ul>
         </form>
     </main>
 </body>
