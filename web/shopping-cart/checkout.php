d86590ac6abe151513bd8c9d9ac3dde6a496aef2
rachelbarnes <bar13059@byui.edu>
2018-10-08 22:05:00 -0600

    added cart styling

--- web/shopping-cart/checkout.php
+++ web/shopping-cart/checkout.php
@@ -53,14 +53,14 @@ session_start();
                 </div>
                 <button type="submit" class="btn btn-primary">Place Order</button>
             </form>
+            <div class="container">
+                <div class="row">
+                    <a class="btn btn-warning" href="view-cart.php">Return to Cart</a>
 
-        </div>
-        <div class="container">
-            <div class="row">
-                <a class="btn btn-warning" href="view-cart.php">Return to Cart</a>
-
+                </div>
             </div>
         </div>
+
     </main>
 </body>
 
