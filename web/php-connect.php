a0fc7679c10449c5e898d1e26d812fbdd7bd2b31
rachelbarnes <bar13059@byui.edu>
2018-10-18 17:46:24 -0600

    fixed database connection php

--- web/php-connect.php
+++ web/php-connect.php
@@ -1,26 +1,27 @@
 <?php
-function get_db()
-{
+function get_db() {
+    $db = NULL;
     try {
+        // default Heroku Postgres configuration URL
         $dbUrl = getenv('DATABASE_URL');
-
-        $dbOpts = parse_url($dbUrl);
-
-        $dbHost = $dbOpts["host"];
-        $dbPort = $dbOpts["port"];
-        $dbUser = $dbOpts["user"];
-        $dbPassword = $dbOpts["pass"];
-        $dbName = ltrim($dbOpts["path"],'/');
-
+        // Get the various parts of the DB Connection from the URL
+        $dbopts = parse_url($dbUrl);
+        $dbHost = $dbopts["host"];
+        $dbPort = $dbopts["port"];
+        $dbUser = $dbopts["user"];
+        $dbPassword = $dbopts["pass"];
+        $dbName = ltrim($dbopts["path"],'/');
+        // Create the PDO connection
         $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-
-        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        // this line makes PDO give us an exception when there are problems, and can be very helpful in debugging!
+        $db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
     }
-    catch (PDOException $ex)
-    {
-        echo 'Error!: ' . $ex->getMessage();
+    catch (PDOException $ex) {
+        // If this were in production, you would not want to echo
+        // the details of the exception.
+        echo "Error connecting to DB. Details: $ex";
         die();
     }
-    return $db
+    return $db;
 }
 ?>
