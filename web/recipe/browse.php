f8c523a47c69f27d3e64f4630f4f30c005ca0b9d
rachelbarnes <bar13059@byui.edu>
2018-10-20 15:03:47 -0600

    fixed user and category info on browse

--- web/recipe/browse.php
+++ web/recipe/browse.php
@@ -12,7 +12,7 @@ if (isset($_GET['recipeSearch']))
     $query = "SELECT r.recipe_title, r.recipe_ingredients, c.recipe_category, u.display_name FROM recipe r INNER JOIN public.category c ON r.recipe_category = c.id INNER JOIN public.user u ON r.user_id = u.id WHERE recipe_title LIKE '%$recipeSearch%'";
 }
 else {
-    $query = 'SELECT r.recipe_title, r.recipe_ingredients, u.display_name FROM recipe r INNER JOIN public.user u ON r.user_id = u.id';
+    $query = 'SELECT r.recipe_title, r.recipe_ingredients, c.recipe_category, u.display_name FROM recipe r INNER JOIN public.category c ON r.recipe_category = c.id INNER JOIN public.user u ON r.user_id = u.id';
 }
 
 $stmt = $db->prepare($query);
