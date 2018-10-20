d6387929308b3f5d21e04537757d9e3ef9e72e1e
rachelbarnes <bar13059@byui.edu>
2018-10-20 15:02:08 -0600

    trying to fix user-recipe

--- web/recipe/browse.php
+++ web/recipe/browse.php
@@ -12,7 +12,7 @@ if (isset($_GET['recipeSearch']))
     $query = "SELECT r.recipe_title, r.recipe_ingredients, c.recipe_category, u.display_name FROM recipe r INNER JOIN public.category c ON r.recipe_category = c.id INNER JOIN public.user u ON r.user_id = u.id WHERE recipe_title LIKE '%$recipeSearch%'";
 }
 else {
-    $query = 'SELECT * FROM recipe';
+    $query = 'SELECT r.recipe_title, r.recipe_ingredients, u.display_name FROM recipe r INNER JOIN public.user u ON r.user_id = u.id';
 }
 
 $stmt = $db->prepare($query);
@@ -61,7 +61,6 @@ $recipes = $stmt->fetchAll(PDO::FETCH_ASSOC);
                     $ingredients = $recipe['recipe_ingredients'];
                     $category = $recipe['recipe_category'];
                     $user = $recipe['display_name'];
-                    var_dump($recipe);
                     echo "<div class='col-md-4'>
                             <div class='card mb-4 shadow-sm'>
                                 <div class='card-body'>
