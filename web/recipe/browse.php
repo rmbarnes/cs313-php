372c23da9e3ad77a7cf190ad65e0bdf7f8d2aefd
rachelbarnes <bar13059@byui.edu>
2018-10-19 23:06:52 -0600

    changed post to get

--- web/recipe/browse.php
+++ web/recipe/browse.php
@@ -5,9 +5,9 @@ $db = get_db();
 
 
 //go through each movie in the result
-if (isset($_POST['recipeSearch']))
+if (isset($_GET['recipeSearch']))
 {
-    $recipeSearch = $_POST['recipeSearch'];
+    $recipeSearch = $_GET['recipeSearch'];
 
     $query = "SELECT r.recipe_title, r.recipe_ingredients, c.recipe_category, u.display_name FROM recipe r INNER JOIN public.category c ON r.recipe_category = c.id INNER JOIN public.user u ON r.user_id = u.id WHERE recipe_title LIKE '%$recipeSearch%'";
 }
@@ -16,7 +16,7 @@ else {
 }
 
 $stmt = $db->prepare($query);
-$stmt->bindValue(':recipeSearch', $recipeSearch, PDO::PARAM_STR);
+//$stmt->bindValue(':recipeSearch', $recipeSearch, PDO::PARAM_STR);
 $stmt->execute();
 $recipes = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
@@ -41,7 +41,7 @@ $recipes = $stmt->fetchAll(PDO::FETCH_ASSOC);
                 <p class="lead my-3 banner-text">We're here to help.</p>
             </div>
 
-            <form method="POST" action="browse.php">
+            <form method="GET" action="browse.php">
                 <div class="col-md-4">
                     <div class="input-group">
                         <input type="text" class="form-control" placeholder="Search by title" name="recipeSearch">
