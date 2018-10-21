b0b7379f93fd1b60369244aa81122b7370c5def8
rachelbarnes <bar13059@byui.edu>
2018-10-20 21:41:38 -0600

    implemented session variable

--- web/recipe/user-recipes.php
+++ web/recipe/user-recipes.php
@@ -15,7 +15,7 @@ $db = get_db();
 //go through each movie in the result
 if (isset($username))
 {
-    $query = "SELECT u.display_name, r.recipe_title FROM public.user u INNER JOIN public.recipe r ON u.id = r.user_id WHERE u.username =  '$username'";
+    $query = "SELECT u.display_name, r.recipe_title FROM public.user u INNER JOIN public.recipe r ON u.id = r.user_id WHERE u.username =  "'$_SESSION['username']'"";
 }
 else {
     $query = 'SELECT * FROM recipe';
