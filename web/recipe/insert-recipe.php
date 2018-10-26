af68ca70a2d1e2ddf0da65b0f1a6933ef98eb4c8
rachelbarnes <bar13059@byui.edu>
2018-10-26 13:32:22 -0600

    testing insert recipe

--- web/recipe/insert-recipe.php
+++ web/recipe/insert-recipe.php
@@ -8,6 +8,7 @@ $db = get_db();
 if(isset($_SESSION['username']))
 {
     $userId = $_SESSION['userId'];
+    var_dump($userId);
     $recipeTitle = htmlspecialchars($_POST['recipeTitle']);
     $ingredients = htmlspecialchars($_POST['ingredients']);
     $cat = htmlspecialchars($_POST['cat']);
@@ -16,7 +17,7 @@ if(isset($_SESSION['username']))
     $query = $db->prepare("INSERT INTO public.recipe(user_id, recipe_title, recipe_ingredients, recipe_category)
                             VALUES (:userId, :recipeTitle, :ingredients,
                             :cat)");
-    $query->bindValue(":userId", $userId, PDO::PARAM_STR);
+    $query->bindValue(":userId", $userId, PDO::PARAM_INT);
     $query->bindValue(":recipeTitle", $recipeTitle, PDO::PARAM_STR);
     $query->bindValue(":ingredients", $ingredients, PDO::PARAM_STR);
     $query->bindValue(":cat", $cat, PDO::PARAM_STR);
