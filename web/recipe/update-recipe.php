f8d34d38e4779a9dec4a1301f4b25c0d85cdb3cb
rachelbarnes <bar13059@byui.edu>
2018-10-26 17:17:02 -0600

    added update functionality

--- web/recipe/update-recipe.php
+++ web/recipe/update-recipe.php
@@ -5,10 +5,14 @@ require('../php-connect.php');
 $db = get_db();
 
 $recipeId = $_POST['id'];
-$recipeTitle = $_POST['recipeTitle'];
-$ingredients = $_POST['ingredients'];
-$cat = $_POST['cat'];
+$recipeTitle = htmlspecialchars($_POST['recipeTitle']);
+$ingredients = htmlspecialchars($_POST['ingredients']);
+$cat = htmlspecialchars($_POST['cat']);
 
+//var_dump($recipeId);
+//var_dump($recipeTitle);
+//var_dump($ingredients);
+//var_dump($cat);
 
 $query = $db->prepare("UPDATE public.recipe
                         SET (recipe_title, recipe_ingredients, recipe_category)
@@ -18,7 +22,7 @@ $query = $db->prepare("UPDATE public.recipe
 $query->bindValue(':recipeId', $recipeId, PDO::PARAM_INT);
 $query->bindValue(':recipeTitle', $recipeTitle, PDO::PARAM_STR);
 $query->bindValue(':ingredients', $ingredients, PDO::PARAM_STR);
-$query->bindValue(':cat', $cat, PDO::PARAM_STR);
+$query->bindValue(':cat', $cat, PDO::PARAM_INT);
 
 $query->execute();
 
