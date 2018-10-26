ef7e1fca6d373ac4eaa7075184e2e53dfa97cd79
rachelbarnes <bar13059@byui.edu>
2018-10-26 16:36:14 -0600

    refactoring create user

---
+++ web/recipe/insert-user.php
@@ -0,0 +1,41 @@
+<?php
+session_start();
+//connect to DB
+require('../php-connect.php');
+$db = get_db();
+
+
+
+
+
+if(isset($_POST['displayName']))
+{
+$displayName = htmlspecialchars($_POST['displayName']);
+$username = htmlspecialchars($_POST['username']);
+$pass = htmlspecialchars($_POST['pass']);
+
+$newPass = password_hash($pass, PASSWORD_DEFAULT);
+
+$query = $db->prepare("INSERT INTO public.user(username, password, display_name)
+VALUES (:username, :password, :displayName)");
+$query->bindValue(":username", $username, PDO::PARAM_STR);
+$query->bindValue(":displayName", $displayName, PDO::PARAM_STR);
+$query->bindValue(":password", $newPass, PDO::PARAM_STR);
+
+    try
+    {
+    $query->execute();
+    }
+    catch(PDOException $Exception)
+    {
+    $msg = "Username already exists";
+    header("location: create-user.php?msg=$msg");
+    }
+
+$userInfo = $query->fetch(PDO::FETCH_ASSOC);
+
+$_SESSION['username'] = $username;
+}
+header('location: browse.php');
+
+?>
