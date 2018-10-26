0c7d76d6452ea9ccfb31cc8205a2bfaca8e4d4ea
rachelbarnes <bar13059@byui.edu>
2018-10-26 15:06:12 -0600

    testing id

--- web/recipe/user-recipes.php
+++ web/recipe/user-recipes.php
@@ -110,9 +110,9 @@ if (isset($_SESSION['username']))
                                                 <p class='card-text'>Category: $category</p>
                                                 <p class='card-text'>Contributor: $user</p>
                                                 <input type='submit' value='Edit' class='btn btn-success'>
-                                                <input type='hidden' name='recipeId' value='$recipeId'>
+                                                <input type='hidden' name='recipeId' value=$recipeId>
                                             </form>
-                                            <a href='delete-recipe.php?id=$recipeId' value='Delete' class='btn btn-danger'></a>
+                                            <a href='delete-recipe.php?id=$recipeId' value='Delete' class='btn btn-danger'>Delete</a>
                                         </div>
                                     </div>
                                 </div>";
