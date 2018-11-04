92ff0ca2ca7e4c9185d9c436582f5a46a27e0ed8
rachelbarnes <bar13059@byui.edu>
2018-11-03 23:23:26 -0600

    testing output

--- web/recipe/meal-plans.php
+++ web/recipe/meal-plans.php
@@ -12,7 +12,7 @@ if (isset($_SESSION['userId']))
               meal_plan_user.display_name AS meal_plan_user_display_name,
               meal_plan.start_date AS start_date,
               meal_plan.end_date AS end_date,
-              json_object_agg(
+              jsonb_object_agg(
                   jsonb_build_object(
                       'recipe_id',                recipe.id,
                       'recipe_title',             recipe.recipe_title
