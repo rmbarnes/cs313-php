2ec4f2f6257eca8fffa96fbdcb5e1b10b6bb78a7
rachelbarnes <bar13059@byui.edu>
2018-11-03 23:53:00 -0600

    testing output

--- web/recipe/meal-plans.php
+++ web/recipe/meal-plans.php
@@ -13,7 +13,7 @@ if (isset($_SESSION['userId']))
               meal_plan.start_date AS start_date,
               meal_plan.end_date AS end_date,
               recipe.id,
-              recipe.recipe_title,
+              recipe.recipe_title
 
             FROM meal_plan
             INNER JOIN meal_plan_recipe
