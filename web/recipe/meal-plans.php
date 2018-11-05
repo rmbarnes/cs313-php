2968118894f877276794b84ef9c5ee00d043ceb1
rachelbarnes <bar13059@byui.edu>
2018-11-05 16:05:50 -0700

    arranging output

--- web/recipe/meal-plans.php
+++ web/recipe/meal-plans.php
@@ -74,12 +74,8 @@ var_dump($mealPlan);
                         foreach ($mealPlan as $plan)
                         {
                             $recipes = json_decode($plan['recipes']);
-                            var_dump($recipes);
 
-                            foreach($recipes as $recipe)
-                            {
-                                echo $recipe->recipe_title . '<br>';
-                            }
+
                             echo "\n";
                             $start = date('M d', strtotime($plan['start_date']));
                             $end = date('M d', strtotime($plan['end_date']));
@@ -88,8 +84,13 @@ var_dump($mealPlan);
                             echo "<div class='col-md-4'>
                             <div class='card mb-4 shadow-sm'>
                                 <div class='card-body'>
-                                    <p class='card-text recipe-title'>$start - $end</p>
-                                    <p class='card-text'>$recipe</p>
+                                    <p class='card-text recipe-title'>$start - $end</p>";
+
+                            foreach($recipes as $recipe)
+                            {
+                                echo $recipe->recipe_title . '<br>';
+                            }
+                            echo "<p class='card-text'>$recipe</p>
                                 </div>
                             </div>
                         </div>";
