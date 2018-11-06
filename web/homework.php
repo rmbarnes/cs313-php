8b8f70c84394a032c685acdcd130c39f2281558d
rachelbarnes <bar13059@byui.edu>
2018-11-06 15:24:21 -0700

    added link to recipes

--- web/homework.php
+++ web/homework.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="utf-8">
     <title>Homework</title>
+    <link rel="stylesheet" type="text/css" href="/style.css" />
     <link rel="stylesheet" type="text/css" href="bootstrap-4.1.3/dist/css/bootstrap.min.css" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="bootstrap-4.1.3/site/docs/4.1/examples/carousel/carousel.css" />
