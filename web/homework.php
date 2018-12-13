b0615bd3d33d7ffc049a95acdd691c87ebc089f1
rachelbarnes <bar13059@byui.edu>
2018-12-12 22:24:47 -0700

    added link to node recipe app

--- web/homework.php
+++ web/homework.php
@@ -27,7 +27,13 @@
         </div>
         <div class="container">
             <div class="row">
-                <a class="btn btn-outline-info" href="recipe/index.php">Recipe App</a>
+                <a class="btn btn-outline-info" href="recipe/index.php">PHP Recipe App</a>
+
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <a class="btn btn-outline-info" href="https://boiling-ravine-83455.herokuapp.com/login">Node Recipe App</a>
 
             </div>
         </div>
